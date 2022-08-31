6d5551804768c8429f93358a473e095d09c02f9a
herdi-nooor <herdinoor369@gmail.com>
2022-08-31 23:03:14 +0800

    add object pooling script

--- Assets/ObjectPoolling/objectpolling.cs
+++ Assets/ObjectPoolling/objectpolling.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class objectpolling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static objectpolling SharedInstance;
+
+    public List<GameObject> pooledObject;
+    public GameObject objectToPooled;
+    public int amountToPool;
+
+    private void Awake() {
+        SharedInstance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start() {
+        pooledObject = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPooled);
+            obj.SetActive(false);
+            pooledObject.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject(){
+        for (int i = 0; i < pooledObject.Count; i++)
+        {
+            if (!pooledObject[i].activeInHierarchy)
+            {
+                return pooledObject[i];
+            }
+        }
+        return null;
+    }
+
+    // object to spawn position and rotation
+    GameObject spawner;
+    public void classCallingPolingObject()
     {
-        
+        // this to get object in pooledObject
+        // this to save object from object pooling
+        GameObject objects = ObjectPooler.SharedInstance.GetPooledObject();
+        // loop to check object
+        if (objects != null)
+        {
+            objects.transform.position = spawner.transform.psition;
+            objects.transform.rotation = spawner.transform.rotation;
+            objects.SetActive(true);
+        }
+
+        //to destroy
+        objects.SetActive(false);
+        // orthis
+        if (objects.gameObject.tag = "destroyer")
+        {
+            if (gameObject.tag == "objectsInPolled")
+            {
+                gameObject.SetActive(false);
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    /// this if you need to incrase amount of objectpooling in runtime condition
+    public bool shoulsExpand = true;
+    public GameObject GetPooledObjectExpand(){
+        for (int i = 0; i < pooledObject.Count; i++)
+        {
+            if (!pooledObject[i].activeInHierarchy)
+            {
+                return pooledObject[i];
+            }
+        }
+        if (shoulsExpand)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPooled);
+            obj.SetActive(false);
+            pooledObject.Add(obj);
+            return obj;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
