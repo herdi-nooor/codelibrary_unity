eb7b07a4c0ce4a7ddae97dc484f9638eeab5aa44
herdin nor <73617545+herdi-nooor@users.noreply.github.com>
2022-09-02 17:00:28 +0800

    Update ExtensionMethod.cs

--- Assets/pubsub/ExtensionMethod.cs
+++ Assets/pubsub/ExtensionMethod.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ExtensionMethod : MonoBehaviour
 {
-    public static Connector GetMessageBus(this gameObject go) {}
+    public static Connector GetMessageBus(this gameObject go) 
+    {
+         var connectorBus = go.GetComponent<BehaviourMessageBus>();
+
+            if (messageBus == null)
+            {
+                //no message bus yet -- make one!
+                messageBus = go.AddComponent<BehaviourMessageBus>();
+            }
+
+            return messageBus;
+    }
+    public static BehaviourMessageBus GetMessageBus(this Component comp)
+    {
+        return comp.gameObject.GetMessageBus();
+    }
 }
