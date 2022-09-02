ea13629f5d970c03538e4525651b25679b1902bc
herdin nor <73617545+herdi-nooor@users.noreply.github.com>
2022-09-02 13:52:44 +0800

    Update Publiser.cs

--- Assets/pubsub/Publiser.cs
+++ Assets/pubsub/Publiser.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class Publiser : MonoBehaviour
 {
+    Connector _connectorBus;
+        public Connector ConnectorBus
+        {
+            get
+            {
+                return _connectorBus;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            //resolve MessageBus before we do anything else:
+            _connectorBus = GetComponent<Connector>();
+
+            if (_connectorBus == null)
+            {
+                //we couldn't find a message bus -- we'll have to
+                //create one
+                _connectorBus = gameObject.AddComponent<Connector>();
+            }
+        }
     void OnAction()
     {
-        /// MessageBus == connector.cs
-        MessageBus.OnAction?.Invoke(this);
+        /// ConnectorBus == connector.cs
+        ConnectorBus.OnAction?.Invoke(this);
     }
 }
